c59e8cb84d6001197d3a971c8e304cacc05eb86f
Viktor Prikolota <VP_ampersand_P@icloud.com>
2019-10-22 20:07:47 +0300

    Initial Commit

---
+++ TripBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripBook
+//
+//  Created by Viktor on 22.10.2019.
+//  Copyright © 2019 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
