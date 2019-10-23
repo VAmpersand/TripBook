c6741fd27a83b58e2db4407c99303f3f81161cba
Viktor Prikolota <VP_ampersand_P@icloud.com>
2019-10-23 19:16:11 +0300

    Edit eventModel

--- TripBook/EventModel.swift
+++ TripBook/EventModel.swift
@@ -11,7 +11,8 @@ import RealmSwift
 class Event: Object {
     
     @objc dynamic var eventName = "Event name"
-    @objc dynamic var descriptionOfEvent = "Description of event"
+    @objc dynamic var eventDescription = "Description of event"
+    @objc dynamic var eventType = "Event type" // Add correct data format(Mb Enum ?! reservationApartment, reservationHotelRoom, departure, transfer )
     
     @objc dynamic var countryName = "Country of destination"
     @objc dynamic var cityName = "City of destination"
@@ -19,13 +20,26 @@ class Event: Object {
     @objc dynamic var buildingNumber = "Building number"
     @objc dynamic var cordinate = "Data: JPS" // Add correct data format
     @objc dynamic var cityMapCach = "Data: Map cash" // Add correct data format
+    @objc dynamic var cityWeather = "JSON data weather: URL request" // Add correct data format
     
     @objc dynamic var placeName = "Place of destination"
     @objc dynamic var placePlan = "Data: JPG, PDF, PNG" // Add correct data format
+    @objc dynamic var airportCode = "Airport code: : URL request" // Add correct data format
     
     @objc dynamic var startEvent = Date()
     @objc dynamic var endEvent = Date()
+    let notification = List<Notification>()
     
     @objc dynamic var ticket = "Data: JPG, PDF, PNG" // Add correct data format
+    @objc dynamic var typeOfDeparture = "Type of departure" // Add correct data format(Mb Enum ?! rentCar, taxi, bus, airplane, ship)
+    @objc dynamic var numberOfDeperture = "Number of departure: URL request" // Add correct data format
+    @objc dynamic var path = "JSON data: Gate, platform, pier: URL request" // Add correct data format
     
 }
+
+class Notification: Object {
+    
+    @objc dynamic var notification = Date()
+    
+}
+
